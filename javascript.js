e82aeec4fb225a307de45155e43d007badfb75e4
Florent9741 <100339623+Florent9741@users.noreply.github.com>
2022-03-20 17:00:56 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Anim click
+window.addEventListener('click', (e) => {
+   // console.log(e);
+
+   const rond = document.createElement('div');
+   rond.className = 'clickAnim';
+   rond.style.top = `${e.pageY - 50}px`;
+   rond.style.left = `${e.pageX - 50}px`;
+   document.body.appendChild(rond);
+   setTimeout(() => {
+      rond.remove();
+   }, 1500)
+})
